f4862155ddd111463a3dbb1ca72820f38f957aac
JoseMV86 <soy.josemv@gmail.com>
2025-05-03 23:43:13 +0200

    Milkonstantan/Clearing ranged trait

--- src/main/java/com/empatic/main/init/modifiers/trait/ClearingModifier.java
+++ src/main/java/com/empatic/main/init/modifiers/trait/ClearingModifier.java
@@ -2,6 +2,7 @@ package com.empatic.main.init.modifiers.trait;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,23 +11,22 @@ import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.build.ConditionalStatModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.mining.BreakSpeedModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
-import slimeknights.tconstruct.tools.stats.ToolType;
+import slimeknights.tconstruct.library.tools.stat.FloatToolStat;
+import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-// TODO: convert into a module
-public class ClearingModifier extends Modifier implements BreakSpeedModifierHook {
-
-    private static final ToolType[] TYPES = { ToolType.MELEE };
+public class ClearingModifier extends Modifier implements BreakSpeedModifierHook, ConditionalStatModifierHook {
 
     @Override
     protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
-        hookBuilder.addHook(this, ModifierHooks.BREAK_SPEED);
+        hookBuilder.addHook(this, ModifierHooks.CONDITIONAL_STAT, ModifierHooks.BREAK_SPEED);
     }
 
     @Override
@@ -35,27 +35,31 @@ public class ClearingModifier extends Modifier implements BreakSpeedModifierHook
     }
 
 
-    /** Gets the boost for the given level and height, can go negative */
+    /** Increase stats if affected by a harmful effect. Cancelled by beneficial effects */
     private static float getBoost(LivingEntity living, ModifierEntry entry, float baseSpeed) {
         List<MobEffectInstance> efectos = new ArrayList<>((living).getActiveEffects());
         boolean fatiga = false;
         boolean herido = false;
         boolean trampas = false;
         for (MobEffectInstance efecto:efectos) {
+            //Cancelled by Mining Fatigue
             if (efecto.getEffect().equals(MobEffects.DIG_SLOWDOWN)){
                 fatiga = true;
             }
         }
         if (!fatiga) {
             for (MobEffectInstance efecto:efectos) {
-                if (!(efecto.getEffect().isBeneficial())){
+                if (efecto.getEffect().getCategory() == MobEffectCategory.HARMFUL){
+                    //You need to be affected by at least one harmful effect
                     herido = true;
-                } else {
+                } else if (efecto.getEffect().getCategory() == MobEffectCategory.BENEFICIAL) {
+                    //You mustn't be affected by beneficial effects. Note that neutral ones don't matter
                     trampas = true;
                 }
             }
             if (herido && !trampas) {
-                return baseSpeed * 2f * entry.getEffectiveLevel();
+                //12.5% boost per level
+                return baseSpeed * 1.125f * entry.getEffectiveLevel();
             }
         }
         return baseSpeed;
@@ -68,5 +72,13 @@ public class ClearingModifier extends Modifier implements BreakSpeedModifierHook
             return;
         }
         event.setNewSpeed(getBoost(event.getEntity(), modifier, event.getNewSpeed()));
+    }
+
+    @Override
+    public float modifyStat(IToolStackView tool, ModifierEntry modifier, LivingEntity living, FloatToolStat stat, float baseValue, float multiplier) {
+        if (stat == ToolStats.DRAW_SPEED) {
+            return getBoost(living, modifier, baseValue);
+        }
+        return baseValue;
     }
 }
